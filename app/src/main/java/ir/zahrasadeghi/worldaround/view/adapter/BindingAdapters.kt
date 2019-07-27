3129fb5063fb8c9f872526f1e894d0cc4e8f6775
ZahraSdg <sdg.zahra@gmail.com>
2019-07-27 14:10:26 +0430

    manage picasso memory

--- app/src/main/java/ir/zahrasadeghi/worldaround/view/adapter/BindingAdapters.kt
+++ app/src/main/java/ir/zahrasadeghi/worldaround/view/adapter/BindingAdapters.kt
@@ -4,6 +4,9 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.paging.PagedList
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Callback
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
 import ir.zahrasadeghi.worldaround.R
 import ir.zahrasadeghi.worldaround.model.RecommendedItem
@@ -20,11 +23,36 @@ object BindingAdapters {
     @BindingAdapter("bind:imageUrl")
     fun loadImage(view: ImageView, imageUrl: String) {
 
-        Picasso.Builder(view.context)
-            .build()
-            .load(imageUrl)
+        val picasso = Picasso.Builder(view.context).build()
+
+        picasso.load(imageUrl)
+            .networkPolicy(NetworkPolicy.OFFLINE)
             .placeholder(R.drawable.ic_image)
             .fit()
-            .into(view)
+            .into(view, object : Callback {
+                override fun onSuccess() {
+                    picasso.shutdown()
+                }
+
+                override fun onError(e: Exception?) {
+                    loadFromNetwork()
+                }
+
+                private fun loadFromNetwork() {
+                    picasso.load(imageUrl)
+                        .placeholder(R.drawable.ic_image)
+                        .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+                        .fit()
+                        .into(view, object : Callback {
+                            override fun onSuccess() {
+                                picasso.shutdown()
+                            }
+
+                            override fun onError(e: Exception?) {
+                                picasso.shutdown()
+                            }
+                        })
+                }
+            })
     }
 }
